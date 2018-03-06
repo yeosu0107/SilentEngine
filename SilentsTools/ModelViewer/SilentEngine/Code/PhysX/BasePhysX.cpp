944bc5f9d97a3eb8edbea060da14bf083dbfd14a
Yeosu0107 <yeosu0107@naver.com>
2018-03-06 15:59:47 +0900

    [모델뷰어] physX 기본세팅
    디버깅용 라이브러리 추가
    기본적인 세팅 적용

--- SilentsTools/ModelViewer/SilentEngine/Code/PhysX/BasePhysX.cpp
+++ SilentsTools/ModelViewer/SilentEngine/Code/PhysX/BasePhysX.cpp
@@ -1,2 +1,55 @@
 #include "stdafx.h"
 #include "BasePhysX.h"
+
+BasePhysX::BasePhysX()
+{
+}
+
+BasePhysX::~BasePhysX()
+{
+}
+
+void BasePhysX::InitPhysics(bool interactive)
+{
+	gFoundation=PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gErrorCallback);
+
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+	//for (PxU32 i = 0; i<5; i++)
+	//	createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);
+
+	if (!interactive)
+		createDynamic(PxTransform(PxVec3(0, 40, 100)), PxSphereGeometry(10), PxVec3(0, -50, -100));
+}
+
+PxRigidDynamic * BasePhysX::createDynamic(const PxTransform & t, const PxGeometry & geometry, const PxVec3 & velocity = PxVec3(0))
+{
+	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
+	dynamic->setAngularDamping(0.5f);
+	dynamic->setLinearVelocity(velocity);
+	gScene->addActor(*dynamic);
+	return dynamic;
+}
--- SilentsTools/ModelViewer/SilentEngine/Code/PhysX/BasePhysX.h
+++ SilentsTools/ModelViewer/SilentEngine/Code/PhysX/BasePhysX.h
@@ -1,5 +1,35 @@
 #pragma once
 
-//#pragma comment(lib, "..\..\lib\SnippetUtils.lib")
 
 #include "..\..\PhysX\PxPhysicsAPI.h"
+using namespace physx;
+
+#define PVD_HOST "127.0.0.1" //비주얼 디버거를 위한 아이피 (로컬아이피)
+
+class BasePhysX
+{
+private:
+	PxDefaultAllocator			gAllocator; //할당자 인터페이스
+	PxDefaultErrorCallback		gErrorCallback; //에러 콜백
+
+	PxFoundation*				gFoundation = nullptr; //
+	PxPhysics*						gPhysics = nullptr; //모든 장면에 적용되는 전역 매개변수 설정 및 공유 개체 생성
+
+	PxDefaultCpuDispatcher*	gDispatcher = nullptr;
+	PxScene*						gScene = nullptr;
+
+	PxMaterial*						gMaterial = nullptr;
+
+	PxPvd*							gPvd = nullptr;
+
+	PxReal							stackZ = 10.0f;
+
+public:
+	BasePhysX();
+	~BasePhysX();
+
+	void InitPhysics(bool interactive);
+
+	PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry,
+		const PxVec3& velocity);
+};
