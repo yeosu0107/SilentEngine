743644e6fad3063263a80145b8b008c4a9477107
hongsukbum <31842972+hongsukbum@users.noreply.github.com>
2018-03-16 23:39:43 +0900

    [Server]패킷 추가 부분 테스트 클라 및 서버
    실수로 깃용 폴더에 잇는 프로젝트로 작업해서 기존거 백업 및 추가작업중인부분 같이 커밋

---
+++ serverframe/ServerFrame2/ServerFrame for kor/stdafx.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+void err_quit(char *msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, (LPCSTR)msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+void err_display(char *msg) {
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
