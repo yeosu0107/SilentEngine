16b663c91021a2a37fd52461dc50a07475dec838
kkw2238 <kkw2238@naver.com>
2019-07-05 14:32:28 +0900

    Update HDRShader.hlsl

--- SilentEngine/hlsl/HDRShader.hlsl
+++ SilentEngine/hlsl/HDRShader.hlsl
@@ -6,72 +6,77 @@
 #include "Sampler.hlsl"
 #include "UnorderedAccess.hlsl"
 
+// ì‚¬ëžŒ ëˆˆì´ ì¸ì‹í•˜ëŠ” ìƒ‰ìƒ ë¯¼ê°ë„
 static const float4 LUM_FACTOR = float4(0.299, 0.587, 0.114, 0);
 
 float3 ToneMApping(float3 color)
 {
+    // í”½ì…€ì˜ Luminance Scaleê³„ì‚°
     float lumScale = dot(color, LUM_FACTOR.xyz);
+    
+    // ToneMapping ê³µì‹
     lumScale *= gMiddleGrey / gAverageValues[0];
     lumScale = (lumScale + (lumScale * lumScale) / gLumWhiteSqr) / (1.0f + lumScale);
 
     return color * lumScale;
-
 }
 
-VS_TEXTURED_OUTPUT VSHDR(uint nVertexID : SV_VertexID)
+VS_TEXTURED_OUTPUT VSPostProcessing(uint nVertexID : SV_VertexID)
 {
     VS_TEXTURED_OUTPUT output = (VS_TEXTURED_OUTPUT) 0.0f;
    
     if (nVertexID == 0)
     {
         output.position = float4(-1.0f, +1.0f, 0.0f, 1.0f);
-        output.uv = (float2(0.0f, 0.0f)); // ½ºÅ©¸° ¿ÞÂÊ À§ 
+        output.uv = (float2(0.0f, 0.0f)); // ìŠ¤í¬ë¦° ì™¼ìª½ ìœ„ 
     }
     if (nVertexID == 1)
     {
-        output.uv = (float2(1.0f, 0.0f)); // ½ºÅ©¸° ¿À¸¥ÂÊ À§
+        output.uv = (float2(1.0f, 0.0f)); // ìŠ¤í¬ë¦° ì˜¤ë¥¸ìª½ ìœ„
         output.position = float4(+1.0f, +1.0f, 0.0f, 1.0f);
 
     }
     if (nVertexID == 2)
     {
-        output.uv = (float2(1.0f, 1.0f)); // ½ºÅ©¸° ¿À¸¥ÂÊ ¾Æ·¡
+        output.uv = (float2(1.0f, 1.0f)); // ìŠ¤í¬ë¦° ì˜¤ë¥¸ìª½ ì•„ëž˜
         output.position = float4(+1.0f, -1.0f, 0.0f, 1.0f);
 
     }
     if (nVertexID == 3)
     {
-        output.uv = (float2(0.0f, 0.0f)); // ½ºÅ©¸° ¿ÞÂÊ À§
+        output.uv = (float2(0.0f, 0.0f)); // ìŠ¤í¬ë¦° ì™¼ìª½ ìœ„
         output.position = float4(-1.0f, +1.0f, 0.0f, 1.0f);
 
     }
     if (nVertexID == 4)
     {
-        output.uv = (float2(1.0f, 1.0f)); // ½ºÅ©¸° ¿À¸¥ÂÊ ¾Æ·¡
+        output.uv = (float2(1.0f, 1.0f)); // ìŠ¤í¬ë¦° ì˜¤ë¥¸ìª½ ì•„ëž˜
         output.position = float4(+1.0f, -1.0f, 0.0f, 1.0f);
 
     }
     if (nVertexID == 5)
     {
-        output.uv = (float2(0.0f, 1.0f)); // ½ºÅ©¸° ¿ÞÂÊ ¾Æ·¡
+        output.uv = (float2(0.0f, 1.0f)); // ìŠ¤í¬ë¦° ì™¼ìª½ ì•„ëž˜
         output.position = float4(-1.0f, -1.0f, 0.0f, 1.0f);
     }
    
     return output;
 };
 
-float4 PSHDR(VS_TEXTURED_OUTPUT input) : SV_Target
+float4 PSPostProcessing(VS_TEXTURED_OUTPUT input) : SV_Target
 {
     float4 finalColor = (float4) 0.0f;
     float2 uv = input.uv;
  
     finalColor = gHDRBuffer[0].Sample(gDefaultSamplerState, uv);
 
-    if (gBloomEnable >= 1.0f)
-        finalColor.xyz += gBloomInput.Sample(gDefaultSamplerState, uv).xyz;
-
+    // í†¤ë§¤í•‘ ì ìš©
     if (gHDREnable >= 1.0f)
         finalColor.xyz = ToneMApping(finalColor.xyz);
+
+    // ë¸”ë£¸ ë¶„í¬ ì ìš©
+    if (gBloomEnable >= 1.0f)
+        finalColor.xyz += gBloomInput.Sample(gDefaultSamplerState, uv).xyz * gBloomScale;
 
     return float4(finalColor.xyz, 1.0f);
 }
